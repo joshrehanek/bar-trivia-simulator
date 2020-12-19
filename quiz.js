0fbe2cdbfd3577fe9650c55d66f62d628123f1fc
Mason M <m8mamiya@gmail.com>
2020-12-19 13:37:19 -0800

    add potential question button append

--- quiz.js
+++ quiz.js
@@ -1,24 +1,39 @@
 //document ready
-$( document ).ready(function() {
-    console.log( "ready!" );
+$(document).ready(function () {
+    console.log("ready!");
 
-//start function
-window.onload = sendApiRequest
+    //start function
+    window.onload = sendApiRequest();
 
-//Api requests
-async function sendApiRequest (){
-    let response = await fetch(`https://opentdb.com/api.php?amount=10&type=multiple`)
-    console.log(response)
-    let data = await response.json()
-    console.log(data)
-    useApiData(data)
-}
-//Pull data from API
-function useApiData (data){
-    const category = data.results[0].category;
-    const question = data.results[0].category;
-    console.log(category)
-    $("#category").text(`Category: ${category}`)
-    $("#question").text(question)
-}
+    //Api requests
+    async function sendApiRequest() {
+        let response = await fetch(`https://opentdb.com/api.php?amount=10&type=multiple`)
+        console.log(response)
+        let data = await response.json()
+        console.log(data)
+        useApiData(data)
+    }
+    //Pull data from API
+    function useApiData(data) {
+        const category = data.results[0].category;
+        const question = data.results[0].question;
+        console.log(category)
+        $("#category").text(`Category: ${category}`)
+        $("#question").text(`Question: ${question}`)
+    }
+
+    // currentQuestion.choices.forEach((answer) => {
+    //     //creates a variable element called tempBtn that is a button
+    //     let tempBtn = document.createElement("button");
+    //     //makes the text content of button an answer from my object array
+    //     tempBtn.textContent = answer;
+    //     //sets tempBtn value to answer
+    //     tempBtn.setAttribute('value', answer);
+    //     //sets tempBtn class to 'button-choice'
+    //     tempBtn.setAttribute('class', 'button-choice');
+    //     //when tempBtn is clicked validateAnswer function is triggered
+    //     tempBtn.onclick = validateAnswer;
+    //     //appends each answer in the form of 'tempBtn' to my answer-zone
+    //     answerZoneEl.appendChild(tempBtn);
+    // });
 });
