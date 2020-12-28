cc22780dc1d287955b318b65d3bf276d88300bdc
Josh Rehanek <joshrehanek@gmail.com>
2020-12-28 14:33:38 -0800

    comments out harry potter code??

--- script.js
+++ script.js
@@ -41,39 +41,39 @@ $.ajax( {
 //         console.error( err );
 //     }
 // };
-console.log
+// console.log
 
 
-const charactersList = document.getElementById( 'charactersList' );
-const searchBar = document.getElementById( 'searchBar' );
-let hpCharacters = [];
+// const charactersList = document.getElementById( 'charactersList' );
+// const searchBar = document.getElementById( 'searchBar' );
+// let hpCharacters = [];
 
-searchBar.addEventListener( 'keyup', ( e ) => {
-    const searchString = e.target.value.toLowerCase();
+// searchBar.addEventListener( 'keyup', ( e ) => {
+//     const searchString = e.target.value.toLowerCase();
 
-    const filteredCharacters = hpCharacters.filter( ( character ) => {
-        return (
-            character.name.toLowerCase().includes( searchString ) ||
-            character.house.toLowerCase().includes( searchString )
-        );
-    } );
-    displayCharacters( filteredCharacters );
-} );
+//     const filteredCharacters = hpCharacters.filter( ( character ) => {
+//         return (
+//             character.name.toLowerCase().includes( searchString ) ||
+//             character.house.toLowerCase().includes( searchString )
+//         );
+//     } );
+//     displayCharacters( filteredCharacters );
+// } );
 
 
-const displayCharacters = ( characters ) => {
-    const htmlString = characters
-        .map( ( character ) => {
-            return `
-            <li class="character">
-                <h2>${character.name}</h2>
-                <p>House: ${character.house}</p>
-                <img src="${character.image}"></img>
-            </li>
-        `;
-        } )
-        .join( '' );
-    charactersList.innerHTML = htmlString;
-};
+// const displayCharacters = ( characters ) => {
+//     const htmlString = characters
+//         .map( ( character ) => {
+//             return `
+//             <li class="character">
+//                 <h2>${character.name}</h2>
+//                 <p>House: ${character.house}</p>
+//                 <img src="${character.image}"></img>
+//             </li>
+//         `;
+//         } )
+//         .join( '' );
+//     charactersList.innerHTML = htmlString;
+// };
 
-loadCharacters();
+// loadCharacters();
